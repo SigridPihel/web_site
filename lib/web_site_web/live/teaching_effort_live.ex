1ba7dd2adc2685aab3e6facc3c99a34ea7a1432d
SigridPihel <sigrid.pihel@yolo.com>
2025-09-18 16:57:11 +0300

    adding a weeks data to socket and form to change weeks amount

--- lib/web_site_web/live/teaching_effort_live.ex
+++ lib/web_site_web/live/teaching_effort_live.ex
@@ -2,7 +2,7 @@ defmodule WebSiteWeb.StudyingEffortLive do
   use WebSiteWeb, :live_view
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, eap_count: 1, studying_hours: 26)
+    socket = assign(socket, eap_count: 1, studying_hours: 26, weeks: 1)
 
     {:ok, socket}
   end
@@ -16,17 +16,25 @@ defmodule WebSiteWeb.StudyingEffortLive do
           +
         </button>
         <div>
-          {@eap_count}
+          ({@eap_count}
         </div>
         &times;
         <div>
-          {@studying_hours}
+          {@studying_hours})
+        </div>
+        /
+        <div>
+          {@weeks})
         </div>
         =
         <div>
-          {@eap_count * @studying_hours}
+          {@eap_count * @studying_hours / @weeks}
         </div>
       </section>
+      <form phx-submit="set-studying-hrs-per-week">
+        <label>Weeks:</label>
+        <input type="number" name="weeks" value={@weeks} />
+      </form>
     </div>
     """
   end
@@ -34,6 +42,11 @@ defmodule WebSiteWeb.StudyingEffortLive do
   def handle_event("add", %{"quantity" => quantity}, socket) do
     socket = update(socket, :eap_count, &(&1 + String.to_integer(quantity)))
 
+    {:noreply, socket}
+  end
+
+  def handle_event("set-studying-hrs-per-week", %{"weeks" => weeks}, socket) do
+    socket = assign(socket, :weeks, String.to_integer(weeks))
     {:noreply, socket}
   end
 end
