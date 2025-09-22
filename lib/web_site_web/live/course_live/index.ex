2ee13b3329627b72fb09b45513628802888114dd
SigridPihel <sigrid.pihel@yolo.com>
2025-09-22 14:44:45 +0300

    refactored code: created functional components course_card and badge

--- lib/web_site_web/live/course_live/index.ex
+++ lib/web_site_web/live/course_live/index.ex
@@ -16,19 +16,35 @@ defmodule WebSiteWeb.CourseLive.Index do
     ~H"""
     <div class="course-index">
       <div class="courses">
-        <div :for={course <- @courses} class="card">
-          <img src={course.image_path} />
-          <h2>{course.title}</h2>
-          <div class="details">
-            <div class="badge">
-              {course.status}
-            </div>
-            <div class="eap">
-              {course.eap} eap
-            </div>
-          </div>
+        <.course_card :for={course <- @courses} course={course} />
+      </div>
+    </div>
+    """
+  end
+
+  attr :course, WebSite.Course, required: true
+
+  def course_card(assigns) do
+    ~H"""
+    <div class="card">
+      <img src={@course.image_path} />
+      <h2>{@course.title}</h2>
+      <div class="details">
+        <.badge status={@course.status} />
+        <div class="eap">
+          {@course.eap} eap
         </div>
       </div>
+    </div>
+    """
+  end
+
+  attr :status, :string, values: ["in progress", "backlog", "done"], default: "backlog"
+
+  def badge(assigns) do
+    ~H"""
+    <div class="rounded-md px-2 py-1 text-xs font-medium uppercase inline-block border text-lime-600 border-lime-600">
+      {@status}
     </div>
     """
   end
